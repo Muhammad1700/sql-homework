ed02f94cbec7fc36e79f631d53ee7a68efa635a7
Muhammad1700 <abdulxakovmuhammad@gmail.com>
2025-06-22 20:49:03 +0500

    Create hw1.sql

---
+++ lesson-1/homework/hw1.sql
@@ -0,0 +1,7 @@
+create database schoolDB
+
+create table Students(studentid int primary key,name varchar(50),age int)
+
+select * from Students
+
+insert into Students values(4,'zokir',23),(5,'jorabek',22),(7,'qobil',20);
